4a7dab000e7b6d50036a0242fc556528d776d1b1
Michael Hickman <mhickman@unm.edu>
2013-11-17 15:11:35 -0700

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117220204) do
+ActiveRecord::Schema.define(version: 20131117220755) do
 
   create_table "addresses", force: true do |t|
     t.integer  "address_id"
@@ -72,6 +72,37 @@ ActiveRecord::Schema.define(version: 20131117220204) do
     t.datetime "updated_at"
   end
 
+  create_table "rfis", force: true do |t|
+    t.string   "requestor_name"
+    t.string   "requestor_agency"
+    t.string   "telephone_number"
+    t.string   "fax_number"
+    t.string   "agency_case_number"
+    t.string   "email_address"
+    t.integer  "priority_id"
+    t.string   "type_of_drug"
+    t.string   "subject_name"
+    t.string   "subject_address"
+    t.string   "subject_business_name"
+    t.string   "subject_alias"
+    t.string   "subject_dob"
+    t.string   "alien_registration_number"
+    t.string   "subject_sex"
+    t.string   "subject_race"
+    t.string   "subject_height"
+    t.string   "subject_weight"
+    t.string   "subject_ssn"
+    t.string   "subject_driver_license_number"
+    t.string   "vehicle_id"
+    t.string   "state_of_registration"
+    t.string   "vehicle_vin"
+    t.string   "subject_telephone_numbers"
+    t.string   "known_criminal_activity"
+    t.string   "information_needed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "siezure_reports", force: true do |t|
     t.string   "taskforce"
     t.string   "agency"
@@ -95,6 +126,16 @@ ActiveRecord::Schema.define(version: 20131117220204) do
     t.string   "pocphone"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "siezures", force: true do |t|
+    t.string   "drug_type"
+    t.float    "amount"
+    t.float    "price"
+    t.string   "currency"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "siezure_report_id"
   end
 
   create_table "subjects", force: true do |t|
@@ -123,8 +164,6 @@ ActiveRecord::Schema.define(version: 20131117220204) do
     t.datetime "updated_at"
   end
 
-<<<<<<< HEAD
-=======
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -143,7 +182,6 @@ ActiveRecord::Schema.define(version: 20131117220204) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
->>>>>>> 23e1498ac766fb16935c6c2d9c064104933d936d
   create_table "vehicles", force: true do |t|
     t.string   "make"
     t.string   "model"
