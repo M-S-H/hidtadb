490430de389f2ce81467ce980f607615b0559938
John-Mark Collins <johnmarkcollins@gmail.com>
2013-12-03 00:22:58 -0700

    Added validation samples and popover samples for live feedback. Also included bootstrap javascript and formatting for more fluid css layouts and aesthetics.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,26 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require_tree .
+//= require bootstrap-affix.js
+//= require bootstrap-alert.js
+//= require bootstrap-button.js
+//= require bootstrap-carousel.js
+//= require bootstrap-collapse.js
+//= require bootstrap-dropdown.js
+//= require bootstrap-modal.js
+//= require bootstrap-popover.js
+//= require bootstrap-scrollspy.js
+//= require bootstrap-tab.js
+//= require bootstrap-tooltip.js
+//= require bootstrap-transition.js
+//= require bootstrap-typeahead.js
 
-
+$(function () {
+  $('.popover-test').popover({ 
+    html : true,
+    title: "This is a test",
+    content: "<b>hello popover world</b>"
+  }).popover('show');
+});
